1072cbae1de8526f872c4c483484aaea4403df07
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-09-22 11:51:34 -0700

    Planning scratch comment.

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,39 @@ pub use component::{Component};
 pub use ecs::{Ecs, EcsTypeId};
 pub use entity::{Entity, EntityParts};
 pub use resource::{Resource};
+
+/*
+
+fn foo_system(
+    mut ecs: EcsBridge<'_>,
+    world: ResMut<World, Required<Immediate>>,
+    
+) {
+}
+
+trait SystemFunction {
+    type Components: ComponentQuery;
+    type Resources: ResourceQuery;
+
+    fn call(mut ecs: EcsBridge<'_>)
+}
+
+SystemData {
+    // The function that is called for this system.
+    system: Box<dyn SystemFunction>,
+    queries: Vec<SystemQuery>,
+}
+
+Ecs {
+    entities: HashMap<EntityId, EntityData>,
+    components: HashMap<ComponentId, ComponentData>,
+    systems: HashMap<SystemId, SystemData>,
+    resources: HashMap<ResourceId, ResourceData>,
+}
+
+EcsQuery {
+    when: When,
+    components: ComponentList,
+    resources: ResourceList,
+}
+*/
